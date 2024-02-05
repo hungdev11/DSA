7586314fbd938a94700157e708450e9d7dbbea1e
HUNG <hungphangialai11@gmail.com>
2024-02-05 20:30:35 +0700

    implement traversal and insert method for singly linked-list

--- Linked_list/Main.java
+++ Linked_list/Main.java
@@ -2,7 +2,17 @@ public class Main{
     public static void main(String[] args)
     {
         SinglyLinkedList sLL = new SinglyLinkedList();
-        sLL.createSinglyLinkedList(5);
+        sLL.insertNode(1, 54);
+        sLL.insertNode(1, 1);
+        sLL.insertNode(2, 2);
+        sLL.insertNode(1, 3);
+        sLL.insertNode(1, 56);
+        sLL.insertNode(6, 4);
+        System.out.println(sLL.size); 
+        System.out.println(sLL.head.value);
         System.out.println(sLL.tail.value);
+        sLL.traversalSinglyLinkedList();
     }
 }
+// 56 3 1 2 54 6  
+// h,          t
--- Linked_list/SinglyLinkedList.java
+++ Linked_list/SinglyLinkedList.java
@@ -15,4 +15,54 @@ public class SinglyLinkedList{
 
         return head;
     }
+    public void traversalSinglyLinkedList()
+    {
+        while(head != null)
+        {
+            System.out.print(head.value + " ");
+            head = head.next;
+        }
+    }
+    public Node insertNode(int position, int nodeValue)
+    {
+        if(position > size + 1 || position < 1)
+        {
+            System.out.println("Wrong position input!, Can't insert value.");
+            return head;
+        }
+        if(head == null)
+        {
+            head = createSinglyLinkedList(nodeValue);
+            return head;
+        }
+        else
+        {
+            Node newNode = new Node();
+            newNode.value = nodeValue;
+            newNode.next = null;
+        
+            if(position == 1)
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+            else if(position == size + 1)
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                Node travelNode = head;
+                for(int i = 1; i < position - 1; i++)
+                {
+                    travelNode = travelNode.next;
+                }
+                newNode.next = travelNode.next;
+                travelNode.next = newNode;
+            }
+        }
+        size++;
+        return head;
+    }
 }
