89866decaaf1eed22a738893e0c04d420abc589a
HUNG <hungphangialai11@gmail.com>
2024-02-06 20:49:33 +0700

    add insert and traversal method

--- Linked_list/CircularDoublyLinkedList/CircularDoublyLinkedList.java
+++ Linked_list/CircularDoublyLinkedList/CircularDoublyLinkedList.java
@@ -20,4 +20,59 @@ public class CircularDoublyLinkedList{
         size = 1;
         return head;
     }
+    public Node insertNode(int position, int nodeValue)
+    {
+        if(position < 1 || position > size +1)
+        {
+            System.out.println("Invalid position");
+            return head;
+        }
+        if(head == null)
+        {
+            return createCDLL(nodeValue);
+        }
+        else
+        {
+            Node newNode = newNode(nodeValue);
+            if(position == 1){
+                head.prev = newNode;
+                newNode.next = head;
+                newNode.prev = tail;
+                head = newNode;
+                tail.next = head;
+            }else if(position == size+1){
+                tail.next = newNode;
+                newNode.prev = tail;
+                newNode.next = head;
+                tail = newNode;
+                head.prev = tail;
+            }else{
+                Node temp = head;
+                for(int i = 1; i<position-1; i++)
+                {
+                    temp = temp.next;
+                }
+                newNode.next = temp.next;
+                temp.next.prev = newNode;
+                temp.next = newNode;
+                newNode.prev = temp;
+            }
+        }
+        size++;
+        return head;
+    }
+    public void traversalCDLL()
+    {
+        Node temp = head;
+        if(temp == null)
+        {
+            System.out.println("Empty linked list!");
+        }
+        do
+        {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }while(temp != tail.next);
+        System.out.println("\n");
+    }
 }
--- Linked_list/CircularDoublyLinkedList/Main.java
+++ Linked_list/CircularDoublyLinkedList/Main.java
@@ -2,9 +2,15 @@ public class Main{
     public static void main(String[] args)
     {
         CircularDoublyLinkedList CDLL = new CircularDoublyLinkedList();
-        CDLL.createCDLL(6);
-        System.out.println(CDLL.head.prev.value);
-        System.out.println(CDLL.head.next.value);
+        CDLL.insertNode(1, 0);
+        CDLL.insertNode(2, 1);
+        CDLL.insertNode(1, 2);
+        CDLL.insertNode(2, 3);
+        CDLL.insertNode(5, 6);
+        CDLL.insertNode(6, 5);
+        CDLL.traversalCDLL();
+        System.out.println(CDLL.size);
 
+        //2 3 0 1 6 5
     }
 }
