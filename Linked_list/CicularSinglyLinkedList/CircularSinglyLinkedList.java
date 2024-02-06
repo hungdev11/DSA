471f5e5c2160b06036ee0d3cd9bc0f938a7611d3
HUNG <hungphangialai11@gmail.com>
2024-02-06 15:55:52 +0700

    add searching method

--- Linked_list/CicularSinglyLinkedList/CircularSinglyLinkedList.java
+++ Linked_list/CicularSinglyLinkedList/CircularSinglyLinkedList.java
@@ -8,9 +8,75 @@ public class CircularSinglyLinkedList{
         Node newNode = new Node();
         newNode.value = nodeValue;
         newNode.next = newNode;
-
         head = tail = newNode;
         size = 1;
         return head;
+    }
+    public Node insertCSLL(int position, int nodeValue)
+    {
+        if(position < 1 || position > size + 1)
+        {
+            System.out.println("Can't insert at that position!");
+            return head;
+        }
+        if(head == null)
+        {
+            return createCSLL(nodeValue);
+        }
+        else{
+            Node newNode = new Node();
+            newNode.value = nodeValue;
+            newNode.next = null;
+
+            if(position == 1)
+            {
+                newNode.next = head;
+                head = newNode;
+                tail.next = newNode;
+            }
+            else if(position == size + 1)
+            {
+                tail.next = newNode;
+                tail = newNode;
+                newNode.next = head;
+            }
+            else
+            {
+                Node travelNode = head;
+                for(int i = 1; i<position-1; i++)
+                {
+                    travelNode = travelNode.next;
+                }
+                newNode.next = travelNode.next;
+                travelNode.next = newNode;
+            }
+        }
+        size++;
+        return head;
+    }
+    public void traversalCSLL()
+    {
+        Node temp = head;
+        if(temp == null) return;
+        do
+        {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }while(temp != tail.next);
+        System.out.println("\n");
+    }
+    public int searchCSLL(int nodeValue)
+    {
+        int foundAt = 0;
+        Node temp = head;
+        do{
+            foundAt++;
+            if(temp.value == nodeValue)
+            {
+                return foundAt;
+            }
+            temp = temp.next;
+        }while(temp != tail.next && temp != null);
+        return -1;
     }
 }
--- Linked_list/CicularSinglyLinkedList/Main.java
+++ Linked_list/CicularSinglyLinkedList/Main.java
@@ -2,8 +2,17 @@ public class Main{
     public static void main(String[] args)
     {
         CircularSinglyLinkedList CSLL = new CircularSinglyLinkedList();
-        CSLL.createCSLL(5);
-        System.out.println(CSLL.head.next.value);
+        CSLL.insertCSLL(1, 3);
+        CSLL.insertCSLL(2, 4);
+        CSLL.insertCSLL(1, 2);
+        CSLL.insertCSLL(1, 6);
+        CSLL.insertCSLL(1, 7);
+        CSLL.insertCSLL(6, 0);
+        System.out.println(CSLL.size);
+        CSLL.traversalCSLL();
+        System.out.println(CSLL.searchCSLL(2));
+        System.out.println(CSLL.searchCSLL(-1));
 
+        //7 6 2 3 4 0
     }
 }
