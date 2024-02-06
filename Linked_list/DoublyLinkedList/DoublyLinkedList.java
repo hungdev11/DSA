88fbb445323158f1e905acf3b06eb0ccef1b7971
HUNG <hungphangialai11@gmail.com>
2024-02-06 19:31:59 +0700

    add searching method for doubly linked list

--- Linked_list/DoublyLinkedList/DoublyLinkedList.java
+++ Linked_list/DoublyLinkedList/DoublyLinkedList.java
@@ -3,15 +3,85 @@ public class DoublyLinkedList{
     public Node tail;
     public int size;
 
-    public Node createDLL(int nodeValue)
+    public Node newNode(int nodeValue)
     {
         Node newNode = new Node();
         newNode.value = nodeValue;
         newNode.prev = null;
         newNode.next = null;
+        return newNode;
+    }
+    public Node createDLL(int nodeValue)
+    {
+        Node newNode = newNode(nodeValue);
         head = newNode;
         tail = newNode;
         size = 1;
         return head;
+    }
+    public Node insertDLL(int position, int nodeValue)
+    {
+        if(position < 1 || position > size+1)
+        {
+            System.out.println("Your postition is invalid!");
+            return head;
+        }
+        if(head == null)
+        {
+            return createDLL(nodeValue);
+        }
+        else
+        {
+            Node newNode = newNode(nodeValue);
+            if(position == 1)
+            {
+                head.prev = newNode;
+                newNode.next = head;
+                head = newNode;
+            }
+            else if(position == size+1)
+            {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            }   
+            else
+            {
+                Node travelNode = head;
+                for(int i = 1; i < position - 1; i++)
+                {
+                    travelNode = travelNode.next;
+                }
+                newNode.next = travelNode.next;
+                travelNode.next.prev = newNode;
+                newNode.prev = travelNode;
+                travelNode.next = newNode;
+            }
+        }
+        size++;
+        return head;
+    }
+    public void traversalDLL()
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+        System.out.println("\n");
+    }
+    public int searchDLL(int nodeValue)
+    {
+        Node temp = head;
+        int foundAt = 0;
+        while(temp != null)
+        {
+            foundAt++;
+            if(temp.value == nodeValue)
+                return foundAt;
+            temp = temp.next;
+        }
+        return -1;
     }
 }
--- Linked_list/DoublyLinkedList/Main.java
+++ Linked_list/DoublyLinkedList/Main.java
@@ -1,6 +1,16 @@
 public class Main{
     public static void main(String[] args)
     {
-        
+        DoublyLinkedList DLL = new DoublyLinkedList();
+        DLL.insertDLL(1, 0);
+        DLL.insertDLL(1, 2);
+        DLL.insertDLL(1, 1);
+        DLL.insertDLL(2, 4);
+        DLL.insertDLL(1, 7);
+        DLL.insertDLL(6, 6);
+        System.out.println(DLL.size);
+        DLL.traversalDLL();
+        System.out.println(DLL.searchDLL(-2));
+        // 7 1 4 2 0 6
     }
 }
