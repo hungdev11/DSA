2f069bace3dcb5beabd477c609c22ee1ef77ac3e
HUNG <hungphangialai11@gmail.com>
2024-03-15 22:24:58 +0700

    floyd warshall imlementation for all pair shortest path problem

--- Graph/WeightedGraph/Main.java
+++ Graph/WeightedGraph/Main.java
@@ -8,23 +8,27 @@ public class Main {
         nodeList.add(new WeightedNode("B", 1));
         nodeList.add(new WeightedNode("C", 2));
         nodeList.add(new WeightedNode("D", 3));
-        nodeList.add(new WeightedNode("E", 4));
-        nodeList.add(new WeightedNode("F", 5));
-        nodeList.add(new WeightedNode("G", 6));
+        // nodeList.add(new WeightedNode("E", 4));
+        // nodeList.add(new WeightedNode("F", 5));
+        // nodeList.add(new WeightedNode("G", 6));
 
         WeightedGraph wg = new WeightedGraph(nodeList);
-        wg.addWeightedEgde(0, 1, 2);
-        wg.addWeightedEgde(0, 2, 5);
-        wg.addWeightedEgde(1, 2, 6);
-        wg.addWeightedEgde(1, 3, 3);
-        wg.addWeightedEgde(1, 4, 3);
-        wg.addWeightedEgde(3, 4, 4);
-        wg.addWeightedEgde(4, 6, 9);
-        wg.addWeightedEgde(2, 5, 8);
-        wg.addWeightedEgde(5, 6, 7);
+        wg.addWeightedEgde(0, 3, 1);
+        wg.addWeightedEgde(0, 1, 8);
+        wg.addWeightedEgde(1, 2, 1);
+        wg.addWeightedEgde(2, 0, 4);
+        wg.addWeightedEgde(3, 1, 2);
+        wg.addWeightedEgde(3, 2, 9);
+        // wg.addWeightedEgde(4, 6, 9);
+        // wg.addWeightedEgde(2, 5, 8);
+        // wg.addWeightedEgde(5, 6, 7);
 
-        System.out.println("Printing Dijkstra form source A");
-        wg.dijkstra(nodeList.get(1));
+        // System.out.println("Printing Dijkstra form source A");
+        // System.out.println("Printing Bellman Ford form source A");
+        // wg.dijkstra(nodeList.get(1));
+        // wg.bellmanFord(nodeList.get(0));
+        System.out.println("Printing weight matrix Floyd-Warshall");
+        wg.floydWarshall();
 
     }
 }
--- Graph/WeightedGraph/WeightedGraph.java
+++ Graph/WeightedGraph/WeightedGraph.java
@@ -46,4 +46,82 @@ public class WeightedGraph {
         first.neighbors.add(second);
         first.weightMap.put(second, weight);
     }
+
+    public void bellmanFord(WeightedNode node) {
+        node.distance = 0;
+        for (int i = 0; i < nodes.size(); i++) {
+            for (WeightedNode currNode : nodes) {
+                for (WeightedNode neighbor : currNode.neighbors) {
+                    if (neighbor.distance > currNode.distance + currNode.weightMap.get(neighbor)) {
+                        neighbor.distance = currNode.distance + currNode.weightMap.get(neighbor);
+                        neighbor.parent = currNode;
+                    }
+                }
+            }
+        }
+        // Checking for negative cycle
+        // Print negative cycle if found
+        System.out.println("Checking for negative cycle...");
+        for (WeightedNode currNode : nodes) {
+            for (WeightedNode neighbor : currNode.neighbors) {
+                if (neighbor.distance > currNode.distance + currNode.weightMap.get(neighbor)) {
+                    System.out.println("Negative cycle found: \n");
+                    System.out.println("Vertex name: " + neighbor.name);
+                    System.out.println("Old cost: " + neighbor.distance);
+                    int newDistance = currNode.distance + currNode.weightMap.get(neighbor);
+                    System.out.println("New cost: " + newDistance);
+                    return;
+                }
+            }
+        }
+        System.out.println("Negative cycle not found!\n");
+        // Print shortest paths for each node
+        for (WeightedNode nodeToCheck : nodes) {
+            System.out.print("Node " + nodeToCheck + ", distance: " + nodeToCheck.distance + ", Path: ");
+            pathPrint(nodeToCheck);
+            System.out.println();
+        }
+    }
+
+    public void floydWarshall() {
+        // Init a matrix
+        int size = nodes.size();
+        int[][] W = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            WeightedNode first = nodes.get(i);
+            for (int j = 0; j < size; j++) {
+                WeightedNode second = nodes.get(j);
+                if (i == j) {
+                    W[i][j] = 0;
+                } else if (first.weightMap.containsKey(second)) {
+                    W[i][j] = first.weightMap.get(second);
+                } else {
+                    W[i][j] = Integer.MAX_VALUE / 10;
+                }
+            }
+        }
+
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (W[i][j] > W[i][k] + W[k][j]) {
+                        W[i][j] = W[i][k] + W[k][j];
+                    }
+                }
+            }
+        }
+        System.out.print("   ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(nodes.get(i).name + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print(nodes.get(i).name + ": ");
+            for (int j = 0; j < size; j++) {
+                System.out.print(W[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
